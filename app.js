fec5adefb3d827dd319772ddbc0e48bf2b7ca387
Romantic <595159090004@dpu.ac.th>
2018-06-09 02:05:50 +0700

    function vs oop

--- app.js
+++ app.js
@@ -163,3 +163,123 @@ const pipe = function(...fns) {
 // );
 
 console.log(prepareString(str));
+
+/**
+ * OOP CODE
+ */
+// constructor function
+function UserRecord(id) {
+  if (this instanceof UserRecord) {
+    this.userId = id;
+    this.questions = [];
+  } else {
+    return new UserRecord(id);
+  }
+}
+
+// prototype propertes and method
+let userProto = {
+  addQuestion: function(qID, response, result, weight) {
+    this.questions.push({
+      qID: qID,
+      response: response,
+      result: result,
+      weight: weight
+    });
+  },
+  get myScore() {
+    let score = 0;
+    for (let i = 0; i < this.questions.length; i++) {
+      let quest = this.questions[i];
+      if (quest.result) {
+        score += quest.weight;
+      }
+    }
+    return score;
+  },
+  get possScore() {
+    let possible = 0;
+    for (let i = 0; i < this.questions.length; i++) {
+      possible += this.questions[i].weight;
+    }
+    return possible;
+  }
+};
+
+UserRecord.prototype = userProto;
+UserRecord.prototype.constructor = UserRecord;
+
+const user1 = new UserRecord(1);
+const user2 = new UserRecord(2);
+
+user1.addQuestion("q1", "answer", true, 1);
+user1.addQuestion("q2", "wrong answer", false, 2);
+
+user2.addQuestion("q1", "answer", true, 1);
+user2.addQuestion("q2", "right answer", true, 2);
+
+co nsole.log("user1 got " + user1.myScore + " out of " + user1.possScore);
+console.log("user2 got " + user2.myScore + " out of " + user2.possScore);
+
+/** Functional code
+ */
+const clone = o => JSON.parse(JSON.stringify(o));
+
+const createUser = function(id) {
+  return {
+    userId: id,
+    questions: []
+  };
+};
+
+const addQuestion = function(user, qID, response, result, weight) {
+  /*const newUser = clone(user);
+    newUser.questions.push({
+        qID: qID,
+        response: response,
+        result: result,
+        weight: weight
+    });
+    return newUser;*/
+  const questions = clone(user.questions);
+  const newQuestion = {
+    qID: qID,
+    response: response,
+    result: result,
+    weight: weight
+  };
+  return {
+    userId: user.userId,
+    questions: [...questions, newQuestion]
+  };
+};
+
+const calcScore = function(user) {
+  return user.questions.reduce(
+    (tot, quest) => tot + (quest.result ? quest.weight : 0),
+    0
+  );
+};
+
+const calcPossible = function(user) {
+  return user.questions.reduce((tot, quest) => tot + quest.weight, 0);
+};
+
+//WE declare a new variable each time so we don't mutate. However, for ease this strict pattern may not make sense to follow always.
+const user3 = createUser(1);
+const user4 = createUser(2);
+
+const user3a = addQuestion(user3, "q1", "answer", true, 1);
+const user3b = addQuestion(user3a, "q2", "wrong answer", false, 2);
+
+const user4a = addQuestion(user4, "q1", "answer", true, 1);
+const user4b = addQuestion(user4a, "q2", "right answer", true, 2);
+
+console.group("functional programming");
+console.log(
+  "user3 got " + calcScore(user3b) + " out of " + calcPossible(user3b)
+);
+console.log(
+  "user4 got " + calcScore(user4b) + " out of " + calcPossible(user4b)
+);
+console.groupEnd();
