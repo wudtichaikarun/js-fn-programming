61cea17d97e393a2adac59bead50ef266bec93aa
Romantic <595159090004@dpu.ac.th>
2018-06-14 13:50:00 +0700

    edit commit wrong type

--- fn-recursion.js
+++ fn-recursion.js
@@ -20,7 +20,7 @@ const factorial = function(num) {
  */
 
 // ES6
-const factorial = num => (num === 1 ? 1 : num * factorial(num - 1));
+const factorial1 = num => (num === 1 ? 1 : num * factorial(num - 1));
 
 /*const factorial2 = function(x) {
   if (x > 0) {
@@ -30,7 +30,37 @@ const factorial = num => (num === 1 ? 1 : num * factorial(num - 1));
   }
 };*/
 
-/** from babe coder invoked 10 times
+/** recursion!!!  is pretty bad code
+ * because execution context created
+ * every time when functon is invoked
+ * you probably has problem out of memory
+ *
+ * USE TAIL CALL OPTIMIZATION INSTEAND OF RECURSION
+ *    5! = 5 * 4 * 3 * 2 * 1
+ *    5! = 5 * 4!
+ *    5! = 20 * 3!
+ *    5! = 60 * 2!
+ *    5! = 120 * 1!
+ */
+
+// const tailCallFactorial = (n, p = 1) => {
+//   if (n <= 1) return p;
+//   else return tailCallFactorial(n - 1, n * p);
+// };
+
+const tailCallFactorial = (n, p = 1) =>
+  n <= 1 ? p : tailCallFactorial(n - 1, n * p);
+/** Tailcall
+ * n = 5, p = 1, n * p = 5;
+ * n = 4, p = 5, n * p = 20;
+ * n = 3, p = 20, n * p = 60;
+ * n = 2, p = 60, n * p = 120;
+ * n = 1, return p
+ * ans 120
+ */
+
+const factorial = x => (x > 0 ? x * factorial2(x - 1) : 1);
+/** Recursion
  *   5 * factorial(4)
  *   5 * 4 * factorial(3)
  *   5 * 4 * 3 factorial(2)
@@ -43,41 +73,5 @@ const factorial = num => (num === 1 ? 1 : num * factorial(num - 1));
  *   120
  */
 
-// from babel coder
-const factorial2 = x => (x > 0 ? x * factorial2(x - 1) : 1);
-
-let result = factorial(10);
-let result2 = factorial2(10);
-console.log(result);
-console.log(result2);
-
-/** recursion!!!  is pretty bad code
- * because execution contenxt creat
- * every time when functon is invoked
- * you probery has problem out of memory
- *
- * USE TAIL CALL OPTIMIZATION INSTEAND OF RECURSION
- *    5! = 5 * 4 * 3 * 2 * 1
- *    5! = 5 * 4!
- *    5! = 20 * 3!
- *    5! = 60 * 2!
- *    5! = 120 * 1!
- */
-
-const tailCallFactorial = (n, p = 1) => {
-  if (n <= 1) return p;
-  else return tailCallFactorial(n - 1, n * p);
-};
-
-const sortTailCallFactorial = (n, p = 1) =>
-  n <= 1 ? p : sortTailCallFactorial(n - 1, n * p);
-/**
- * n = 5, p = 1, n * p = 5;
- * n = 4, p = 5, n * p = 20;
- * n = 3, p = 20, n * p = 60;
- * n = 2, p = 60, n * p = 120;
- * n = 1, return p
- * ans 120
- */
 console.log(tailCallFactorial(5));
-console.log(sortTailCallFactorial(5));
+console.log(factorial(5));
